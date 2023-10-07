44baa3e2592a0987294007047ba5b550198ded20
rex-9 <htetnaing0814@gmail.com>
2023-10-07 21:58:19 +0630

    update triangle

--- triangle.rb
+++ triangle.rb
@@ -13,8 +13,26 @@
 # and
 #   about_triangle_project_2.rb
 #
+def equality_check x, y, arr
+  result = x == y ? arr << true : arr << false
+  result
+end
+
 def triangle(a, b, c)
   # WRITE THIS CODE
+  array = []
+  equality_check a, b, array
+  equality_check b, c, array
+  equality_check a, c, array
+
+  count = array.count(true)
+  if count == 1 
+    :isosceles 
+  elsif count == 3
+    :equilateral
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
